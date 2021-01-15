4f1d6b316926460d10c9c45e4831c6c30116f07e
ManuMW <manumw21@gmail.com>
2021-01-15 15:22:59 +0530

    wakawaka

--- main.py
+++ main.py
@@ -16,9 +16,10 @@ def index():
         if 'email' in request.form and 'password' in request.form:
             email = request.form['email']
             password = request.form['password']
-            cursor = mysql.connector.connect(host="localmain", user="root", password="root", port="3306")
-            cursor.execute("SELECT * FROM user WHERE email=%s AND password=%s",(email,password))
-            info = cursor.fetchone()
+            pt = mysql.connector.connect(host="localmain", user="root", passwd="roo1t")
+            ndb = pt.cursor()
+            ndb.execute("SELECT * FROM user WHERE email=%s AND password=%s",(email,password))
+            info = ndb.fetchone()
             if info['email'] == email and info['password'] == password:
                 return "Login successfull"
             else:
