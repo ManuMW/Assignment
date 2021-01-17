78f08d88643fc089f8f7e2c14103ea5675cabdba
ManuMW <manumw21@gmail.com>
2021-01-17 20:02:58 +0530

    Search and post box

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-from flask import Flask, render_template, request, session, redirect, url_for, g, make_response
-
+from flask import Flask, render_template, request, session, redirect, url_for, app
 from utils.db_utils import  writeToDatabase, readFromDatabase
+from datetime import timedelta
 
 app = Flask(__name__)
 app.secret_key = "wakawaka"
 
+@app.before_request
+def make_session_permanent():
+    session.permanent = True
+    app.permanent_session_lifetime = timedelta(minutes=2)
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    if "username" in session:
+        return render_template("/homepage.html")
     return render_template("login.html")
 
 
@@ -35,28 +42,27 @@ def login():
     if len(result) == 0:
         return "Invalid Login Credentials"
     else:
-        # username = readFromDatabase(f"SELECT Name FROM user WHERE email='{email}' AND password='{password}' limit 1")
-        username = result[0][1]
-        session["username"] = username
-        return redirect(url_for(".username"))
 
-@app.route("/homepage", methods=['GET'])
+        user = result[0][1]
+
+        session["username"] = user
+        return redirect(url_for("username"))
+
+@app.route("/profile", methods=['GET'])
 def username():
-    if isLoggedIn():
-        user = session["username"]
-        print (user)
-        return render_template("homepage.html", context = {"name": user})
-    else:
-        return render_template("/login.html")
-
-def isLoggedIn():
     if "username" in session:
-        return True
-    return False
+        user_id = session["username"]
+        print (user_id)
+        return render_template('/profile.html')
+    else:
+        return redirect('/')
+
 
 @app.route('/logout')
 def logout():
-
+    session.pop("username")
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
